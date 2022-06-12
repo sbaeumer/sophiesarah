bf9b40800efed2e818f00cd49ae4c87ddf7b4647
sbaeumer <105556833+sbaeumer@users.noreply.github.com>
2022-06-12 11:58:18 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,27 @@
-let poti = 0
+input.onButtonPressed(Button.A, function () {
+    basic.setLedColor(0x00ff00)
+})
 let winkel = 0
-let gesetzterWinkel = 0
+let feuchtigkeit = 0
+pins.analogWritePin(AnalogPin.P2, 0)
+pins.servoWritePin(AnalogPin.C17, 90)
+basic.setLedColor(0x00ff00)
+basic.pause(10000)
+basic.setLedColor(0xff0000)
 basic.forever(function () {
-    poti = pins.analogReadPin(AnalogPin.P1)
-    winkel = poti * (180 / 1023)
-    basic.showNumber(winkel)
-    if (gesetzterWinkel != winkel) {
-        pins.servoWritePin(AnalogPin.P2, winkel)
-        gesetzterWinkel = winkel
+    if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showIcon(IconNames.Sad)
     }
+    feuchtigkeit = pins.analogReadPin(AnalogPin.P1)
+    pins.analogWritePin(AnalogPin.P2, feuchtigkeit)
+    winkel = pins.map(
+    pins.analogReadPin(AnalogPin.C16),
+    0,
+    1023,
+    10,
+    170
+    )
+    pins.servoWritePin(AnalogPin.C17, winkel)
 })
